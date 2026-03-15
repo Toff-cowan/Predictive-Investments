f98a8ec23e91333b71327e96158ac3abe18f4ee0
Toff-cowan <cowanchristoff24@gmail.com>
2026-03-15 06:50:25 -0500

    staged changes

--- Name_Pending/server/src/index.ts
+++ Name_Pending/server/src/index.ts
@@ -73,6 +73,7 @@ app.get("/api/csv", (req, res) => {
   res.sendFile(path.resolve(filePath));
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
